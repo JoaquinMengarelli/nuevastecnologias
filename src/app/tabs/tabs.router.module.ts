040b61e76f7b4752bdb6b33d6b1cb6e015d9919e
Joaquin <joaquinmengarelli@gmail.com>
2020-12-05 21:10:31 -0300

    xd

---
+++ src/app/tabs/tabs.router.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+import { NewsPage } from '../news/news.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'news',
+        outlet: 'news',
+        component: NewsPage
+      },
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/(news:news)',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TabsPageRoutingModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+
+export const environment = {
+  production: false,
+  apiUrl: 'https://newsapi.org/v2',
+  apiKey: '11ebb33f5ee5424ea4249bcebdc86e9d'
+};
+
